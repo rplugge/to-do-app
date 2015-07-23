af60e7592b6438c8da9671bcdfd970c8b7ac8176
Robert Plugge <robmplugge@hotmail.com>
2015-07-23 14:28:07 -0500

    Fixed log-in redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      redirect_to dashboard_path(@user.id)
+      redirect_to login
     else
       render "new" 
     end
